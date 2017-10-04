8b4d153f70e9c5e63881ad04cc8304e7c49e91a7
Jakob Holderbaum <jakob@holderbaum.io>
2017-10-04 23:28:49 +0200

    Add simple diceware passphrase generator

---
+++ diceware/diceware.go
@@ -0,0 +1,45 @@
+package diceware
+
+import (
+        "strings"
+        "errors"
+        "crypto/rand"
+        "math/big"
+
+        "github.com/holderbaum/diceware/wordlists/eff_short_wordlist2"
+        "github.com/holderbaum/diceware/wordlists/diceware_german"
+)
+
+func PickRandomElement(array []string) string {
+  nBig, err := rand.Int(rand.Reader, big.NewInt(int64(len(array))))
+  if err != nil {
+    panic(err)
+  }
+  n := nBig.Int64()
+  return array[n]
+}
+
+func PickWordlist(name string) ([]string, error) {
+  switch name {
+    case "eff":
+      return eff_short_wordlist2.Wordlist, nil
+    case "diceware-german":
+      return diceware_german.Wordlist, nil
+    default:
+      return nil, errors.New("I am really sad :(")
+  }
+}
+
+func Generate(length uint32, id string) (string, error) {
+  var result []string
+  var wordlist, err = PickWordlist(id)
+  if err != nil {
+    return "", err
+  }
+
+  for i := uint32(0); i < length; i++ {
+    result = append(result, PickRandomElement(wordlist))
+  }
+
+  return strings.Join(result, " "), nil
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+        "fmt"
+        "flag"
+        "strconv"
+
+        "github.com/holderbaum/diceware/diceware"
+)
+
+func parseLength(args []string) uint32 {
+        if len(args) != 1 {
+                return 6
+        }
+        if s, err := strconv.ParseUint(args[0], 10, 32); err == nil {
+                return uint32(s)
+        }
+        return 6
+}
+
+func main() {
+        var wordlistType string;
+        var phraseLength uint32;
+
+        flag.StringVar(&wordlistType, "wordlist", "eff", "which wordlist to use")
+        flag.Parse()
+        phraseLength = parseLength(flag.Args())
+
+        var passphrase, err = diceware.Generate(phraseLength, wordlistType)
+        if err == nil {
+                fmt.Println(passphrase)
+        } else {
+                fmt.Println(err)
+        }
+}
